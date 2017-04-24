77eff29c94138dfc2460d3f121512fbf3f343f2d
jjmonasterio <jmonasterio@cafpower.com>
2017-04-24 12:16:14 +0200

    Add files via upload
    Añadida la relación "bus_ut"

--- configuration/views/bus/map.js
+++ configuration/views/bus/map.js
@@ -131,8 +131,7 @@ function(doc) {
 				rel_id: doc._id, 
 				rel_coll: doc.collection,
 				version: doc.bus.version,
-				bus : doc.bus,
-				is_source : doc.is_source
+				bus : doc.bus
 			};  
 			
 			emit([doc.bus.id, doc.bus.version, 3], value_bus);
@@ -151,5 +150,40 @@ function(doc) {
 			emit([doc.bus.id, doc.bus.version, 3], value_car);			
 		}
 		
-	}  
+	}
+	
+	if ("bus_ut" === doc.collection) {
+    
+		var value = {
+			_id: doc._id
+		};
+		
+		emit([doc.bus.id, doc.bus.version, 104], value);
+		
+		if (doc.bus)
+		{		
+			var value_bus = {
+				_id: doc.bus.id,
+				rel_id: doc._id, 
+				rel_coll: doc.collection,
+				version: doc.bus.version,
+				bus : doc.bus
+			};  
+			
+			emit([doc.bus.id, doc.bus.version, 4], value_bus);
+		}
+		
+		if (doc.ut)
+		{
+			var value_ut = {
+				_id: doc.ut.id,
+				rel_id: doc._id, 
+				rel_coll: doc.collection,
+				version: doc.ut.version,
+				ut : doc.ut
+			}; 
+
+			emit([doc.bus.id, doc.bus.version, 4], value_ut);			
+		}		
+	}	
 }
